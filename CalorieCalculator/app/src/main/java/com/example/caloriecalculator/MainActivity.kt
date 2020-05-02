07adff1873b6d14219cde7adf7094c31b78cdb83
Piotr Sumara <piotrsumara@piotrs-macbook-air.home>
2020-05-02 09:32:28 +0200

    m

--- CalorieCalculator/app/src/main/java/com/example/caloriecalculator/MainActivity.kt
+++ CalorieCalculator/app/src/main/java/com/example/caloriecalculator/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.caloriecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +17,22 @@ class MainActivity : AppCompatActivity() {
 
         val sexSpinner: Spinner
         sexSpinner = findViewById<Spinner>(R.id.sexspinner);
+
+        var option: String
+        val options = arrayOf("Man", "Woman")
+
+
+        sexSpinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, options)
+        sexSpinner.onItemSelectedListener = object : OnItemSelectedListener{
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                option = options.get(position)
+            }
+
+        }
     }
 }
