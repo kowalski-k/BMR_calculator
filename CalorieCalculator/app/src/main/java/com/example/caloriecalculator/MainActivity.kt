956189172a0115cfd2a835d02f5aa643d89716f7
Piotr Sumara <piotrsumara@piotrs-macbook-air.home>
2020-05-03 16:04:19 +0200

    Poprawiony dostęp do zmiennych

--- CalorieCalculator/app/src/main/java/com/example/caloriecalculator/MainActivity.kt
+++ CalorieCalculator/app/src/main/java/com/example/caloriecalculator/MainActivity.kt
@@ -7,33 +7,31 @@ import android.view.View
 import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
 
-class MainActivity : AppCompatActivity() {
+abstract class MainActivity : AppCompatActivity() {
 
     private lateinit var calculateButton: Button
+    private lateinit var sexSpinner: Spinner
+    private lateinit var   activitySpinner: Spinner
+    private lateinit var option_sex: String
+    private lateinit var option_activity: String
+    private lateinit var heightEditText: EditText
+    private lateinit var weightEditTextView: EditText
+    private lateinit  var ageEditText: EditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sexSpinner: Spinner
-        sexSpinner = findViewById<Spinner>(R.id.sex_spinner)
-        sexSpinner.prompt
 
-        val activitySpinner: Spinner
+        sexSpinner = findViewById<Spinner>(R.id.sex_spinner)
+
         activitySpinner = findViewById(R.id.activity_spinner)
 
-        var option_sex: String
-        var option_activity: String
-
-
-        var heightEditText: EditText
         heightEditText = findViewById(R.id.height_edit_text)
 
-        var weightEditTextView: EditText
         weightEditTextView = findViewById(R.id.weight_edit_text)
 
-        var ageEditText: EditText
         ageEditText = findViewById(R.id.age_edit_text)
 
         calculateButton = findViewById(R.id.calculate_button)
@@ -78,6 +76,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
+    print(option_sex)
     }
+
 }
