912722d231513f2e7dce36c5a0dba9d4790a79a8
Jakub Kowalski <jakubkowalski38@wp.pl>
2020-05-07 11:24:02 +0200

    Added functions to counting BMR and CPM

--- CalorieCalculator/app/src/main/java/com/example/caloriecalculator/MainActivity.kt
+++ CalorieCalculator/app/src/main/java/com/example/caloriecalculator/MainActivity.kt
@@ -10,11 +10,13 @@ import android.widget.AdapterView.OnItemSelectedListener
 class MainActivity : AppCompatActivity() {
 
     private lateinit var calculateButton: Button
-    private lateinit var option_sex: String
+    private lateinit var option_gender: String
     private lateinit var option_activity: String
     private lateinit var heightEditText: EditText
     private lateinit var weightEditTextView: EditText
     private lateinit var ageEditText: EditText
+    private var BMR = 0.0
+    private var CPM = 0
 
 
 
@@ -22,12 +24,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sexSpinner: Spinner
-        sexSpinner = findViewById<Spinner>(R.id.sex_spinner)
-        sexSpinner.prompt
+        val genderSpinner: Spinner
+        genderSpinner = findViewById<Spinner>(R.id.gender_spinner)
+        genderSpinner.prompt
 
         val activitySpinner: Spinner
         activitySpinner = findViewById(R.id.activity_spinner)
+
 
 
         heightEditText = findViewById(R.id.height_edit_text)
@@ -43,11 +46,11 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        ArrayAdapter.createFromResource(this, R.array.sex_array, android.R.layout.simple_spinner_item)
+        ArrayAdapter.createFromResource(this, R.array.gender_array, android.R.layout.simple_spinner_item)
             .also { adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            sexSpinner.adapter = adapter}
+                genderSpinner.adapter = adapter}
 
-        sexSpinner.onItemSelectedListener = object : OnItemSelectedListener{
+        genderSpinner.onItemSelectedListener = object : OnItemSelectedListener{
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 TODO("Not yet implemented")
@@ -55,7 +58,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 if (parent != null) {
-                    option_sex = parent.getItemAtPosition(position) as String
+                    option_gender = parent.getItemAtPosition(position) as String
                 }
             }
         }
@@ -76,6 +79,37 @@ class MainActivity : AppCompatActivity() {
 
                 }
             }
+        }
+
+        fun calculateBMR(): Double {
+            if(option_gender == "Man"){
+                BMR = 66 + (13.7 * weightEditTextView.text.toString().toDouble()) + (5 * heightEditText.text.toString().toDouble()) - (6.76 * ageEditText.text.toString().toDouble())
+            }
+            else{
+                BMR = 655 + (9.6 * weightEditTextView.text.toString().toDouble()) + (1.8 * heightEditText.text.toString().toDouble()) - (4.7 * ageEditText.text.toString().toDouble())
+            }
+            return BMR
+        }
+        fun calculateCPM(): Int {
+            if(option_activity == "Sedetary: no exercise"){
+                CPM = (BMR * 1.2).toInt()
+            }
+            else if(option_activity == "Light: exercise 2-3 times/week"){
+                CPM = (BMR * 1.35).toInt()
+            }
+            else if(option_activity == "Moderate: exercise 4-5 times/week"){
+                CPM = (BMR * 1.55).toInt()
+            }
+            else if(option_activity == "Active: daily exercise or intense exercise 3-4 times/week"){
+                CPM = (BMR * 1.75).toInt()
+            }
+            else if(option_activity == "Very active: intense exercise 6-7 times/week") {
+                CPM = (BMR * 1.95).toInt()
+            }
+            else{
+                CPM = (BMR * 2.2).toInt()
+            }
+            return CPM
         }
         
 
